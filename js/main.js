e5c566fa50fff27b3446472507d6862277928b9d
dr-maguigan <104376156+dr-maguigan@users.noreply.github.com>
2022-12-02 10:44:29 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -29,10 +29,8 @@ var dotIcon = L.Icon.extend({
 			var orangedot = new dotIcon({iconUrl: 'orange_dot.png'})
 			var reddot = new dotIcon({iconUrl: 'red_dot.png'})
 			var greendot = new dotIcon({iconUrl: 'green_dot.png'})
-			
-			var br = <br>;
 
-var pdtfc = L.marker([34.566576613097574, -100.1963707590198], {icon: greendot}).bindPopup("Station Name: Prairie Dog Town Fork of the Red River near Childress, TX" + br + "Current height: " + childress + br + "Current Stage: Normal" + br + "Historic Data: " + br + '<a href = "https://dr-maguigan.github.io/Red-River-Watershed/Prairie-Dog-Town-Fork-Childress.html"><img src= "Childress.PNG"</a>', {maxWidth: "200px"}).addTo(map);
+var pdtfc = L.marker([34.566576613097574, -100.1963707590198], {icon: greendot}).bindPopup('<h6>Station Name:</h6><br><p>Prairie Dog Town Fork of the Red River near Childress, TX</p><br><h6>Current height: </h6><p>' + childress + '<p><br><h6>Current Stage: </h6><br><p>Normal</p><br><h6>Historic Data: </h6><br><a href = "https://dr-maguigan.github.io/Red-River-Watershed/Prairie-Dog-Town-Fork-Childress.html"><img src= "Childress.PNG"</a>', {maxWidth: "200px"}).addTo(map);
 
 			
 
