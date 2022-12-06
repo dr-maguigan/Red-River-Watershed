57c9fd97086bcc5eab45ba4e75ed904c5bba86e6
dr-maguigan <104376156+dr-maguigan@users.noreply.github.com>
2022-12-06 08:27:26 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -38,7 +38,8 @@ function getData () {
     	}
 	});
 };
-
+console.log(childress);
+console.log(childressCond);
 var pdtfc = L.marker([34.566576613097574, -100.1963707590198], {icon: greendot}).bindPopup('<h6>Station Name:</h6><br><p>Prairie Dog Town Fork of the Red River near Childress, TX</p><br><h6>Current height: </h6><p>' + childress + ' ft<p><br><h6>Current Stage: </h6><br><p>' + childressCond + '</p><br><h6>Historic Data: </h6><br><a href = "https://dr-maguigan.github.io/Red-River-Watershed/Prairie-Dog-Town-Fork-Childress.html"><img src= "Childress.PNG"</a>', {maxWidth: "200px"}).addTo(map);
 
 			
